fe0ed85c535ad060ff3852e6b91488e524d1072e
ABHISHEK VERMA <104015898+AbhiiiDev@users.noreply.github.com>
2024-01-02 19:08:19 +0530

    #ADDED-MANUAL-LIVE-CHAT-SEND

--- src/components/WatchContainer.js
+++ src/components/WatchContainer.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { closeMenu } from '../utils/appSlice';
 import { useSearchParams } from 'react-router-dom';
 import CommentContainer from './CommentContainer';
 import LiveContainer from './LiveContainer';
+import { addMessage } from '../utils/chatSlice';
 
 
 
@@ -16,8 +17,7 @@ const WatchContainer = () => {
   const [searchParam] = useSearchParams();
   console.log(searchParam.get("v"))
 
-
-
+const [text,setText]=useState("");
   return (
     <>
       <div className='p-2 ml-20 flex flex-row'>
@@ -27,6 +27,18 @@ const WatchContainer = () => {
 <div className=''>
 
       <LiveContainer/>
+      <form onSubmit={((e)=>
+      {e.preventDefault();
+      dispatch(addMessage({
+        name:"Abhishek",
+        message:text
+      }))
+      setText("");}
+  
+      )}>
+        <input className='ml-5 w-72  p-1 border border-black' type="text" value={text} onChange={(e)=>setText(e.target.value)}/>
+        <button className='border border-black ml-1 p-1'>Send</button>
+      </form>
 </div>
 
 
